7c2217ed611abf3bc545ac8c4b447645610d5fca
周鹏飞 <1015528172@qq.com>
2016-09-07 15:41:24 +0800

    第二个版本

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,17 +8,74 @@
  *
  * Main module of the application.
  */
-angular
-    .module('yeomanApp', ['ui.router']).config(function($stateProvider){
-  	    $stateProvider.state("main",{
-	  		url:"main",
-	  		templateUrl:'../views/main.html',
-	  		controller:"main"
-	  	}).state("about",{
-	  		url:"about",
-	  		templateUrl:'../views/about.html',
-	  		controller:"about"
-	  	})
-    });
+//angular.module("yeomanApp", ["ui.router"]).config(function($stateProvider){
+//	    $stateProvider.state("main",{
+//	  		url:"/main",
+//	  		templateUrl:"views/main.html",
+//	  		controller:"main",
+//	  		abstract: true,
+//	  	}).state("product",{
+//	  		url:"/product",
+//	  		templateUrl:"views/product.html",
+//	  		controller:"product",
+//	  		abstract: true,
+//	  	}).state("news",{
+//	  		url:"news",
+//	  		templateUrl:'views/news.html',
+//	  		controller:"news"
+//	  	}).state("about",{
+//	  		url:"about",
+//	  		templateUrl:'views/about.html',
+//	  		controller:"about"
+//	  	}).state("case",{
+//	  		url:"case",
+//	  		templateUrl:'views/case.html',
+//	  		controller:"case"
+//	  	}).state("contact",{
+//	  		url:"contact",
+//	  		templateUrl:'views/contact.html',
+//	  		controller:"contact"
+//	  	})
+//  });
 
+ angular.module("yeoman",["ui.router"]).config(function($stateProvider){
+            $stateProvider.state("main",{
+                templateUrl:"../views/main.html",
+                url:"/main"
+            }).state("product",{
+                url:"/product",
+                templateUrl:"../views/product.html"
+            }).state("family",{
+                url:"/product/family",
+                templateUrl:"../views/family.html"
+            }).state("fever",{
+                url:"/product/fever",
+                templateUrl:"../views/fever.html"
+            }).state("screen",{
+                url:"/product/screen",
+                templateUrl:"../views/screen.html"
+            }).state("meeting",{
+                url:"/product/meeting",
+                templateUrl:"../views/meeting.html"
+            }).state("news",{
+                url:"/news",
+                templateUrl:"../views/news.html"
+            }).state("dynamic",{
+                url:"/news/dynamic",
+                templateUrl:"../views/dynamic.html"
+            }).state("information",{
+                url:"/news/information",
+                templateUrl:"../views/information.html"
+            }).state("about",{
+                url:"/about",
+                templateUrl:"../views/about.html"
+            }).state("case",{
+                url:"/case",
+                templateUrl:"../views/case.html"
+            }).state("contact",{
+                url:"/contact",
+                templateUrl:"../views/contact.html"
+            })
 
+        })
+                .controller("b",function(){})
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,7 +7,7 @@
  * # MainCtrl
  * Controller of the yeomanApp
  */
-angular.module('yeomanApp')
+angular.module('yeomanApp',['ui.router'])
   .controller('main', function () {
-   
+      
   });
